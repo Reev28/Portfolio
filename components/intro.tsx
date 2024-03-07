cfdb9d51b84368eea2cd0b789c9242383a150bc4
Reev28 <reev.dsouza@yahoo.com>
2024-03-08 03:30:39 +0530

    data change

--- components/intro.tsx
+++ components/intro.tsx
@@ -45,7 +45,7 @@ const {setActiveSection,settimeOfLastClick} = useActiveSectionContext();
             >
             <span className="font-bold">Hello, I'm Reev.</span> I'm a{" "}
             <span className="font-bold">full-stack developer</span> with{" "}
-            <span className="font-bold">2 years</span> of experience. I enjoy
+            <span className="font-bold">1 years</span> of experience. I enjoy
             building <span className="italic">sites</span>. My focus is{" "}
             <span className="underline">React (Next.js)</span>.
             </motion.p>
